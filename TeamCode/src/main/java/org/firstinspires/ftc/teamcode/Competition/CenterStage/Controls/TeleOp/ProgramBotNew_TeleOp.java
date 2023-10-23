e31a5c3fe3738d521678f7dc1c489ecef9237f75
omtCoder <olivia.tindall@mtbethelchristian.org>
2023-10-23 17:37:44 -0400

    Program Bot TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Controls/TeleOp/ProgramBotNew_TeleOp.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode.Competition.CenterStage.Controls.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Competition.CenterStage.Robots.BlueBot;
+import org.firstinspires.ftc.teamcode.Competition.CenterStage.Robots.ProgramBot_New;
+
+@TeleOp(name="ProgramBot")
+public class ProgramBotNew_TeleOp extends ProgramBot_New {
+
+    public class BlueBot_TeleOp extends OpMode {
+        double leftStickYVal;
+        double leftStickXVal;
+        double rightStickXVal;
+        double rightStickYVal;
+
+
+        double frontLeftSpeed;
+        double frontRightSpeed;
+        double rearLeftSpeed;
+        double rearRightSpeed;
+
+        double powerThreshold = 0;
+        double speedMultiply = 1;
+
+
+        // Construct the physical robot object
+        public org.firstinspires.ftc.teamcode.Competition.CenterStage.Robots.ProgramBot_New ProgramBot_New = new ProgramBot_New();
+
+        @Override
+        public void init() {
+            ProgramBot_New.initRobot(hardwareMap);
+        }
+
+        public void init_loop() {}
+
+        public void start() {}
+
+        @Override
+        public void loop() {
+            speedControl();
+            drive();
+            telemetryOutput();
+
+        }
+        public void drive() {
+
+            leftStickYVal = -gamepad1.left_stick_y;
+            leftStickYVal = Range.clip(leftStickYVal, -1, 1);
+            leftStickXVal = gamepad1.left_stick_x;
+            leftStickXVal = Range.clip(leftStickXVal, -1, 1);
+            rightStickXVal = gamepad1.right_stick_x;
+            rightStickXVal = Range.clip(rightStickXVal, -1, 1);
+
+            frontLeftSpeed = leftStickYVal + leftStickXVal + rightStickXVal;
+            frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+
+            frontRightSpeed = leftStickYVal - leftStickXVal - rightStickXVal;
+            frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+
+            rearLeftSpeed = leftStickYVal - leftStickXVal + rightStickXVal;
+            rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+
+            rearRightSpeed = leftStickYVal + leftStickXVal - rightStickXVal;
+            rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+            if (frontLeftSpeed <= powerThreshold && frontLeftSpeed >= -powerThreshold) {
+                frontLeftSpeed = 0;
+                ProgramBot_New.frontLeftMotor.setPower(frontLeftSpeed);
+            } else {
+                ProgramBot_New.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
+            }
+
+            if (frontRightSpeed <= powerThreshold && frontRightSpeed >= -powerThreshold) {
+                frontRightSpeed = 0;
+                ProgramBot_New.frontRightMotor.setPower(frontRightSpeed);
+            } else {
+                ProgramBot_New.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
+            }
+
+            if (rearLeftSpeed <= powerThreshold && rearLeftSpeed >= -powerThreshold) {
+                rearLeftSpeed = 0;
+                ProgramBot_New.rearLeftMotor.setPower(rearLeftSpeed);
+            } else {
+                ProgramBot_New.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
+            }
+
+            if (rearRightSpeed <= powerThreshold && rearRightSpeed >= -powerThreshold) {
+                rearRightSpeed = 0;
+                ProgramBot_New.rearRightMotor.setPower(rearRightSpeed);
+            } else {
+                ProgramBot_New.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
+            }
+        }
+        // Telemetry Controls Method for EndGame Extension/Lifting and Rotation
+        public void telemetryOutput() {
+
+            telemetry.addData("pwr", "FL mtr: " + frontLeftSpeed);
+            telemetry.addData("pwr", "FR mtr: " + frontRightSpeed);
+            telemetry.addData("pwr", "RL mtr: " + rearLeftSpeed);
+            telemetry.addData("pwr", "RR mtr: " + rearRightSpeed);
+            telemetry.update();
+        }
+        // Driving Speed Control Method
+        public void speedControl() {
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Robots/ProgramBot_New.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Competition.CenterStage.Robots;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Competition.CenterStage.Drivetrains.MecanumDrive;
+
+public class ProgramBot_New extends MecanumDrive {
+
+    // Hardware Mapping Variables
+    public HardwareMap hwBot = null;
+
+    //Gyro Variables
+    public BNO055IMU imu;
+    public Orientation angles;
+    public Acceleration gravity;
+    public final double SPEED = .3;
+    public final double TOLERANCE = .4;
+
+
+    // Constructors
+    public ProgramBot_New(){}
+
+    // Initialization Method
+    public void initRobot(HardwareMap hwMap) {
+        hwBot = hwMap;
+
+        // Drivetrain Motors HW Mapping
+        frontLeftMotor = hwBot.dcMotor.get("front_left_motor");//Port 0 - Control
+        frontRightMotor = hwBot.dcMotor.get("front_right_motor");//Port 1 - Control
+        rearLeftMotor = hwBot.dcMotor.get("rear_left_motor");//Port 2 - Control
+        rearRightMotor = hwBot.dcMotor.get("rear_right_motor");//Port 3 - Control
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+
+}
+
