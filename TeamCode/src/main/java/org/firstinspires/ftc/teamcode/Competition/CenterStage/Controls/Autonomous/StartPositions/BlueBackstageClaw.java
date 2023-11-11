685f4dcaadf50deb9ddf69711c4c5e89506d5b96
bni-blue <bniblue@mtbethelchristian.org>
2023-11-10 21:03:11 -0500

    Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Controls/Autonomous/StartPositions/BlueBackstageClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Controls/Autonomous/StartPositions/BlueBackstageClaw.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.Competition.CenterStage.Controls.Autonomous.StartPositions;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Disabled
+@Autonomous(name = "Blue:Backstage:Claw")
 public class BlueBackstageClaw {
 }
