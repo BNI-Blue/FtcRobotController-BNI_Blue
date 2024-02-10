d1fdfda6eec77281f29f78b8d9aa2acc51c03e3a
bni-blue <bniblue@mtbethelchristian.org>
2024-02-10 08:27:42 -0500

    speed acceleration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Controls/Autonomous/StartPositions/SpeedIncreaseTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/CenterStage/Controls/Autonomous/StartPositions/SpeedIncreaseTest.java
@@ -6,17 +6,25 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.Competition.CenterStage.Controls.Autonomous.AutoMain;
 
 @Disabled
-@Autonomous(name = "Speed Increase Test")
+@Autonomous(name = "Speed Acceleration Test")
 
 public class SpeedIncreaseTest extends AutoMain {
+
     @Override
+
     public void runOpMode() throws InterruptedException {
+
         Bot.initRobot(hardwareMap);
         Bot.setLinearOp(this);
 
         waitForStart();
+
         while(opModeIsActive()){
-            //Bot.speedAcceleration(10, .6);
+            sleep(1000);
+            Bot.speedAcceleration(10, .6);
+
+            requestOpModeStop();
         }
+        idle();
     }
 }
